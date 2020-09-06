4eb0fcf08ea2bb2c5462b4e2f7320404359e3675
Servesus <jesusgarrocho1998@gmail.com>
2020-09-06 20:22:59 +0200

    Heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,7 +11,22 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import django_heroku
+	ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+	BASEURL = 'https://septiembre-primera-prueba.herokuapp.com' (la url de la aplicacion)
+	(todas serian igual que BASEURL EN APIS)
+	APIS = {
+    		'authentication': 'https://septiembre-primera-prueba.herokuapp.com', 
+    		'base': 'https://septiembre-primera-prueba.herokuapp.com',
+    		'booth': 'https://septiembre-primera-prueba.herokuapp.com',
+    		'census': 'https://septiembre-primera-prueba.herokuapp.com',
+    		'mixnet': 'https://septiembre-primera-prueba.herokuapp.com',
+   		'postproc': 'https://septiembre-primera-prueba.herokuapp.com',
+    		'store': 'https://septiembre-primera-prueba.herokuapp.com',
+    		'visualizer': 'https://septiembre-primera-prueba.herokuapp.com',
+    		'voting': 'https://septiembre-primera-prueba.herokuapp.com',
+		}
+	django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +40,7 @@ SECRET_KEY = '^##ydkswfu0+=ofw0l#$kv^8n)0$i(qd&d&ol#p9!b$8*5%j1+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
